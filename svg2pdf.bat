338146a01e93febe0e96f421ee0c5ae5170e5836
jc0a20 <64551060+jc0a20@users.noreply.github.com>
2020-05-04 02:58:25 +0900

    discarded

--- svg2pdf.bat
+++
@@ -1,4 +0,0 @@
-dir .\export_svg\*.svg /b > .\export_svg\inkscape_export_pdf_list_svg.txt
-for /f "tokens=1 delims=." %%a in ( .\export_svg\inkscape_export_pdf_list_svg.txt ) do ( 
-"C:\Program Files\Inkscape\\inkscape.com" -f .\export_svg\%%a.svg -A .\export_pdf\%%a.pdf
- )
